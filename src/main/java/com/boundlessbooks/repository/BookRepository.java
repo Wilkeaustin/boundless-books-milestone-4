b87fc1613483d2aee3c0ea3506ed07a2d1fef0e5
Austin Wilke <99009551+Wilkeaustin@users.noreply.github.com>
2026-09-07 22:52:43 -0500

    Remove conflicting method and clean up BookRepository

--- src/main/java/com/boundlessbooks/repository/BookRepository.java
+++ src/main/java/com/boundlessbooks/repository/BookRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-<<<<<<< HEAD
+
 	List<Book> findByTitleContainingIgnoreCase(String title);
-=======
-	List<Book> findByTitleIgnoreCase(String title, String author);
->>>>>>> branch 'main' of https://github.com/Wilkeaustin/boundless-books.git
 
 	List<Book> findByGenreIgnoreCase(String genre);
 }
